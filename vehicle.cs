2e7dc74a77b805d092a694269cbf20d3272142bd
Vagner de Souza <76208231+inaciovagner@users.noreply.github.com>
2021-01-23 12:44:56 +0000

    Add files via upload

---
+++ vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace new_class_1
+{
+    class vehicle
+    { 
+        public void PressHorn()
+        {
+            Console.WriteLine("Peep peep");
+        }
+        static void Main(string[] args)
+        {
+            vehicle car = new vehicle();
+            car.PressHorn(); // Outputs "Peep peep"
+
+        }
+    }
+}
+
